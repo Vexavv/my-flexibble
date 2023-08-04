b163f9b2522f625ad6029e4a61b968817a1f656d
Vitalii <vexavv2022@gmail.com>
2023-08-04 12:44:51 +0300

    refresh

--- grafbase/grafbase.config.ts
+++ grafbase/grafbase.config.ts
@@ -1,20 +1,20 @@
-import { g, auth, config } from '@grafbase/sdk'
+import { g, config, auth } from '@grafbase/sdk';
 
 // @ts-ignore
-const User = g.model('User',{
-    name:g.string().length({min:2, max:20}),
+const User = g.model('User', {
+    name: g.string().length({ min: 2, max: 100 }),
     email: g.string().unique(),
     avatarUrl: g.url(),
     description: g.string().length({ min: 2, max: 1000 }).optional(),
     githubUrl: g.url().optional(),
-    linkedInUrl: g.url().optional(),
+    linkedinUrl: g.url().optional(),
     projects: g.relation(() => Project).list().optional(),
-}).auth((rules)=>{
+}).auth((rules) => {
     rules.public().read()
 })
 
 // @ts-ignore
-const Project = g.model('Project',{
+const Project = g.model('Project', {
     title: g.string().length({ min: 3 }),
     description: g.string(),
     image: g.url(),
@@ -29,10 +29,11 @@ const Project = g.model('Project',{
 
 const jwt = auth.JWT({
     issuer: 'grafbase',
-    secret:g.env('NEXTAUTH_SECRET')
+    secret:  g.env('NEXTAUTH_SECRET')
 })
+
 export default config({
-  schema: g,
+    schema: g,
     auth: {
         providers: [jwt],
         rules: (rules) => rules.private()
